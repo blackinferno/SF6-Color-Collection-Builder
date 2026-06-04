28630d6315d63a9b162cf9540e2be2aa60b58272
blackinferno <blackinferno4@gmail.com>
2026-06-04 14:33:53 +0900

    Bump version to 0.1.1

--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ from PySide6.QtCore import QByteArray, QSettings
 
 
 APP_NAME = "SF6 Color Collection Builder"
-APP_VERSION = "0.1.0"
+APP_VERSION = "0.1.1"
 ORG_NAME = "MarshialLaw"
 PROJECT_ROOT = Path(getattr(sys, "_MEIPASS", Path(__file__).resolve().parents[1]))
 APP_DATA_ROOT = (
